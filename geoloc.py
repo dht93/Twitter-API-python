9ac595714c4f0f9eb83d78438f1cf82076f0eb92
Dhruv Thakur <dhruvt93@gmail.com>
2015-09-05 22:33:23 +0530

    Update and rename loc_using_geopy.py to geoloc.py

---
+++ geoloc.py
@@ -0,0 +1,12 @@
+from geolocation.google_maps import GoogleMaps
+
+key='your_google_maps_api_key'
+
+def coordinates(address):
+    coord=[]
+    google_maps=GoogleMaps(api_key=key)
+    location=google_maps.search(location=address)
+    my_location=location.first()
+    coord.append(my_location.lat)
+    coord.append(my_location.lng)
+    return coord
--- loc_using_geopy.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-
-def coordinates(address):
-    geolocator=Nominatim()
-    location=geolocator.geocode(address)
-    coord=[]
-    coord.append(round(location.latitude,2))
-    coord.append(round(location.longitude,2))
-    return coord
