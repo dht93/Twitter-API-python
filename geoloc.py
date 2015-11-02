00dfdd5fe04a94676fefe43402d4be63c3f35048
Dhruv Thakur <dhruvt93@gmail.com>
2015-11-02 17:13:11 +0530

    Update geoloc.py

--- geoloc.py
+++ geoloc.py
@@ -1,7 +1,6 @@
 from geolocation.google_maps import GoogleMaps
 
-key='your_google_maps_api_key'
-
+key='your_google_maps_api_key'  #enter your google maps api key
 def coordinates(address):
     coord=[]
     google_maps=GoogleMaps(api_key=key)
